77ab9b736f1c538a290aa71ea86d236fbc35fed3
tatev2907 <alaverdyan.tatev29@gmail.com>
2020-04-11 21:00:04 +0400

    m

---
+++ Homework/h1a.py
@@ -0,0 +1,8 @@
+input_string = input("Enter a list elements separated by space: ")
+userList = input_string.split()
+print("user list is: ", userList)
+s=input("Enter compearing element: ")
+for i in userList:
+    if s== i:
+        userList.remove(i)
+print("Output list is: ",userList)

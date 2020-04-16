e42196191e9d51bc174683246a7ee0e1fe3bbbfc
tatev2907 <alaverdyan.tatev29@gmail.com>
2020-04-16 21:18:02 +0400

    practical

--- week1/practical1.py
+++ week1/practical1.py
@@ -6,3 +6,18 @@ def last_digit(number):
         return 0
     else:
         return ((number * number) + last_digit(number - 1) )%10
+
+
+def is_polyndrome(strr):
+    listt = []
+    for i in range(len(strr)):
+        if (strr[i] in listt):
+            listt.remove(strr[i])
+        else:
+            listt.append(strr[i])
+    if (len(strr) % 2 == 0 and len(listt) == 0 or \
+            (len(strr) % 2 == 1 and len(listt) == 1)):
+        return True
+    else:
+        return False
+print(is_polyndrome("ababb"))

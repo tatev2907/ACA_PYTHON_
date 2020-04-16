2ff56df16b3b541088671ccf8ac2080fc07cfd29
tatev2907 <alaverdyan.tatev29@gmail.com>
2020-04-16 21:19:32 +0400

    practical

--- week1/practical1.py
+++ week1/practical1.py
@@ -1,11 +1,12 @@
 def implication3(a, b, c):
-    return  not a or (not b or c)
+    return not a or (not b or c)
+
 
 def last_digit(number):
-    if(number == 0):
+    if (number == 0):
         return 0
     else:
-        return ((number * number) + last_digit(number - 1) )%10
+        return ((number * number) + last_digit(number - 1)) % 10
 
 
 def is_polyndrome(strr):
@@ -15,9 +16,8 @@ def is_polyndrome(strr):
             listt.remove(strr[i])
         else:
             listt.append(strr[i])
-    if (len(strr) % 2 == 0 and len(listt) == 0 or \
-            (len(strr) % 2 == 1 and len(listt) == 1)):
+    if (len(strr) % 2 == 0 and len(listt) == 0 or (len(strr) % 2 == 1 and len(listt) == 1)):
         return True
     else:
         return False
-print(is_polyndrome("ababb"))
+

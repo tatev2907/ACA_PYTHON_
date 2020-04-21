75eee0b78a3b2248a0483a227f36466251235942
tatev2907 <alaverdyan.tatev29@gmail.com>
2020-04-21 19:04:46 +0400

    homework1.py

--- week1/practical1.py
+++ week1/practical1.py
@@ -1,22 +1,5 @@
-def implication3(a, b, c):
-    return not a or (not b or c)
-
-
-def last_digit(number):
-    if (number == 0):
-        return 0
-    else:
-        return ((number * number) % 10 + last_digit(number - 1)) % 10
-
-
-def is_polyndrome(strr):
-    listt = []
-    for i in range(len(strr)):
-        if (strr[i] in listt):
-            listt.remove(strr[i])
-        else:
-            listt.append(strr[i])
-    if (len(strr) % 2 == 0 and len(listt) == 0 or (len(strr) % 2 == 1 and len(listt) == 1)):
-        return True
-    else:
-        return False
+def recursive_or(test_list):
+    print(set(test_list).sorted())
+recursive_or([False, False, False, False, True, False])
+def quick_or(test_list):
+    return any(test_list)

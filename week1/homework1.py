e4ca0987b47b58847a4f339ea3a03dec985be805
tatev2907 <alaverdyan.tatev29@gmail.com>
2020-04-21 11:43:53 +0400

    homework1.py

--- week1/homework1.py
+++ week1/homework1.py
@@ -1,7 +1,6 @@
 def all_positive(*args):
     return any([i < 0 for i in args])
 
-
 def xor3(a, b, c):
     k = not (a and b) and (a or b)
     return not (k and c) and (k or c)
@@ -12,22 +11,20 @@ def mirror_string(s):
     for i in range(len(s)):
         p = ord(s[i])
         if p > 96 and p < 123:
-            print(i)
             s[i] = chr(122 - (p - ord('a')))
         elif p > 64 and p < 91:
             s[i] = chr(90 - (p - ord('A')))
     return "".join(s)
 
 
-def bit_concat(*args):
-    val = [f'{i:08b}' for i in args]
+def bit_concat(ls):
+    val = [f'{i:08b}' for i in ls]
     j = 8
     s = ''
     for i in val:
-        s += (i[j - 2:j])
+        s=i[j - 2:j]+s
         j -= 2
     return int(s, 2)
-
 
 def binary_sum(a, b):
     return int(a, 2) + int(b, 2)
